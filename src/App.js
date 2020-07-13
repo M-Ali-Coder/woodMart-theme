cf0de71a8abf83098afa465d748a97eb90e8c83c
M-Ali-Coder <m.ali.coder.93@gmail.com>
2020-07-13 17:46:03 +0200

    Adding AppHeaderNav component with pretty smoothly transitions

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from "react";
 import "./scss/App.scss";
 import TopBar from "./components/TopBar";
 import AppMainHeader from "./components/AppMainHeader";
+import AppHeaderNav from "./components/AppHeaderNav";
 
 function App() {
   return (
     <div className="App">
       <TopBar />
       <AppMainHeader />
+      <AppHeaderNav />
     </div>
   );
 }
