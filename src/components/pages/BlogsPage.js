88e389c53583118812a91d2b1f7380392c3de6f9
M-Ali-Coder <m.ali.coder.93@gmail.com>
2020-07-15 22:40:49 +0200

    Adding a AppPagesBanner component

--- src/components/pages/BlogsPage.js
+++ src/components/pages/BlogsPage.js
@@ -1,6 +1,7 @@
 import React from "react";
 import PostBlogItem from "../PostBlogItem";
 import LATEST_NEWS from "../../data/latestNews";
+import AppPagesBanner from "../AppPagesBanner";
 
 class BlogsPage extends React.Component {
   constructor(props) {
@@ -12,11 +13,13 @@ class BlogsPage extends React.Component {
   }
   render() {
     const { posts } = this.state;
+    const blogBackground =
+      "https://woodmartcdn-cec2.kxcdn.com/wp-content/uploads/2015/12/blog-title-bg5.jpg";
 
     return (
-      <div className="blog-page-wrapper container">
-        <div>Banner</div>
-        <div className="content">
+      <div className="blog-page-wrapper">
+        <AppPagesBanner title="Woodmart Blog" blogBackground={blogBackground} />
+        <div className="content container">
           {posts.map(({ id, ...otherProps }) => (
             <PostBlogItem key={id} {...otherProps} />
           ))}
